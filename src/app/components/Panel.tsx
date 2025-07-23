c935f01fe7c4a325539fcf2056318e4043ae5569
Shreya <shreyashaw1705@gmail.com>
2025-07-24 00:33:58 +0530

    bg-gradient

--- src/app/components/Panel.tsx
+++ src/app/components/Panel.tsx
@@ -33,7 +33,7 @@ const Panel = () => {
           className="rounded-xl object-cover shadow-lg"
         />
         <div className="absolute bottom-4 left-4 flex gap-2 z-10">
-            <p className='bg-zinc-950 rounded-xl p-2 flex items-center'>
+            <p className=' rounded-xl p-2 flex items-center'>
                 <Cctv className="w-4 h-4 mr-1 text-red-500" />
          {activeIncident ? activeIncident.cammera.name: "No Camera Selected"}
             </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,20 @@ import Panel from "./components/Panel";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
+      <div
+        className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-64 pointer-events-none z-[-2]"
+        style={{
+          background: "radial-gradient(ellipse at center top, #fcce19 0%, transparent 70%)",
+          opacity: 0.6,
+        }}
+      />
+
       <Navbar/>
       <Panel/>
     </div>
   );
 }
 
-// TODO: background gardient
 // TODO: try out the optional section
 // TODO: change the logo
